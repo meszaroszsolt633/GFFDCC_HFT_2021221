224d5770547e39be6a1df37c702e35f5464d1fec
razor <razor@DESKTOP-L9VF93M>
2021-10-11 00:16:30 +0200

    Add project files.

---
+++ GFFDCC_HFT_2021221.Client/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace GFFDCC_HFT_2021221.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ GFFDCC_HFT_2021221.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GFFDCC_HFT_2021221.Data
+{
+    public class Class1
+    {
+    }
+}
